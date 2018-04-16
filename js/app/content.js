ff5dd897dd3a92eeb74d77d5c2208ff517b862bb
luke-elwood <luke.elwood@gmail.com>
2018-04-16 16:15:20 -0300

    extension

---
+++ js/app/content.js
@@ -0,0 +1,21 @@
+﻿chrome.extension.onMessage.addListener(function (request, sender, response) {
+	if (request.action == 'pageElements') {
+			var pageElements = {};
+			document.querySelectorAll('*').forEach(function(e){
+				if(pageElements[e.nodeName]){
+					if(e.id){
+						pageElements[e.nodeName].push(e.id);
+					}else if(e.className){
+						pageElements[e.nodeName].push(e.className);
+					}
+				}else{
+					if(e.id){
+						pageElements[e.nodeName] = [e.id];
+					}else if(e.className){
+						pageElements[e.nodeName] = [e.className];
+					}
+				}
+			});
+			response(pageElements);
+	}
+});
---
+++ js/app/popup.js
@@ -0,0 +1,27 @@
+﻿myApp.service('domElements', function() {
+	this.getInfo = function(callback) {
+		var model = {};
+		chrome.tabs.query({'active': true},
+		function (tabs) {
+			if (tabs.length > 0)
+			{
+					model.title = tabs[0].title;
+					model.url = tabs[0].url;
+					chrome.tabs.sendMessage(tabs[0].id, { 'action': 'pageElements' }, function (response) {
+							model.pageElements = response;
+							callback(model);
+					});
+			}
+		});
+	};
+});
+
+myApp.controller("ngController", function ($scope, domElements) {
+    domElements.getInfo(function (info) {
+        $scope.pageElements = info.pageElements;
+        $scope.$apply();
+    });
+});
+
+
+
